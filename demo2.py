cb47d52f04d2a3d6fa9c821cf09aab4ba798c993
zhanghm <1545696299@qq.com>
2020-06-02 00:55:27 +0800

    111

---
+++ demo2.py
@@ -0,0 +1,11 @@
+a =  {"name":"李四","password":"123321"}
+
+zh = input("请输入账号:")
+mm = input("请输入密码:")
+
+for i in a:
+    if zh == i.get("name"):
+        i.get("password") = mm
+    else:
+        a.update("name") = zh
+        a.update("password") = mm
